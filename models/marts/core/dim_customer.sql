52e4eea21aab3a433d010304b370599806759c9f
Sebastian Wiesner <sebastian.wiesner@nimbusintelligence.com>
2023-04-04 09:34:20 +0000

    Finished "Analyses and Seeds"

--- models/marts/core/dim_customer.sql
+++ models/marts/core/dim_customer.sql
@@ -1,13 +1,13 @@
 with customers as (
-
     select * from {{ ref('stg_customers')}}
-
 ),
 
 orders as (
-
     select * from {{ ref('fct_orders') }}
+),
 
+employees as (
+    select * from {{ ref('employees') }} 
 ),
 
 customer_orders as (
@@ -33,6 +33,7 @@ final as (
         customers.customer_id,
         customers.first_name,
         customers.last_name,
+        employees.employee_id is not null as is_employee,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
@@ -41,6 +42,7 @@ final as (
     from customers
 
     left join customer_orders using (customer_id)
+    left join employees using (customer_id)
 
 )
 
